b17e0ea3218ba5bbbb98b4e6ed3bf33963a3e00c
j0s3-s4nch3z <frogapp2019@gmail.com>
2020-03-26 22:57:54 -0300

    comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './app.module#AppModule' },
+  { path: '', loadChildren: './page/login/login.module#LoginPageModule' },
   { path: 'login', loadChildren: './page/login/login.module#LoginPageModule' },
   { path: 'menu', loadChildren: './page/menu/menu.module#MenuPageModule' },
   { path: 'salir', loadChildren: './page/salir/salir.module#SalirPageModule' },
