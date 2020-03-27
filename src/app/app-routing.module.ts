0935d85b9e72a470c196855f222c2378b51797b4
j0s3-s4nch3z <frogapp2019@gmail.com>
2020-03-27 13:44:17 -0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './app.module#AppComponent' },
+  { path: '', loadChildren: './app.module#AppModule' },
   { path: 'login', loadChildren: './page/login/login.module#LoginPageModule' },
   { path: 'menu', loadChildren: './page/menu/menu.module#MenuPageModule' },
   { path: 'salir', loadChildren: './page/salir/salir.module#SalirPageModule' },
